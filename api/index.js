a4cfd75f7fedbf5a7f792485b5dd8375f635c37e
codepanther <pezechigozirim@gmail.com>
2023-02-28 18:40:38 +0100

    Server - connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,20 +1,32 @@
+require('dotenv').config();
+require('express-async-errors');
+
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
+const connectDB = require('./db/connect');
 
 const app = express();
 
-app.use(morgan('combined'))
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.get('/', function (req, res) {
-  res.send('hello, world')
-})
+  res.send('hello, world');
+});
 
-app.get('/some', function ( req, res) {
-  res.send('some what?')
-})
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log('app running on 5000'))
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(PORT, () =>
+      console.log(`app running on http://localhost:${PORT}`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
