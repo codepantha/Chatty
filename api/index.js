80d8d2d916fce68024c4968fdec002cf7070addc
codepanther <pezechigozirim@gmail.com>
2023-03-19 20:51:18 +0100

    Server - set up websocket server connection

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const ws = require('ws');
+
 const connectDB = require('./db/connect');
 
 // custom middleware
@@ -13,6 +15,7 @@ const errorHandlerMiddleware = require('./middleware/errorHandler');
 // routes
 const authRouter = require('./routes/auth.routes');
 const userRouter = require('./routes/user.routes');
+const { decodeToken } = require('./utils/helper');
 
 const app = express();
 
@@ -31,9 +34,34 @@ const PORT = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(PORT, () =>
+    const server = app.listen(PORT, () =>
       console.log(`app running on http://localhost:${PORT}`)
     );
+
+    // set up websocket connection
+    const wsServer = new ws.WebSocketServer({ server });
+    wsServer.on('connection', (connection, req) => {
+      const cookies = req.headers.cookie;
+      // let's ensure we get our token that starts with 'token='
+      // incase there are more cookies in the headers
+      // note: multiple cookies are separated by ';'
+      const cookieString = cookies
+        .split(';')
+        .find((cookie) => cookie.startsWith('token='));
+      if (cookieString) {
+        const token = cookieString.split('=')[1];
+
+        if (token) {
+          const { userId, username } = decodeToken(token);
+
+          connection.userId = userId;
+          connection.username = username;
+        }
+      }
+      
+      console.log([...wsServer.clients].map(client => client.username));
+    });
+
   } catch (e) {
     console.log(e);
   }
