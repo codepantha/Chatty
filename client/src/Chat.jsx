cefe3a2ddfacc9e6f6ddfef23a2e540c6a07ff9b
codepanther <pezechigozirim@gmail.com>
2023-09-26 01:42:48 +0100

    Display online users except logged in user -m "Create 'Logo' component"

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,11 +1,15 @@
-import React, { useEffect, useState } from 'react';
-import { HiOutlineChat } from 'react-icons/hi';
+import React, { useContext, useEffect, useState } from 'react';
+
 import Avatar from './Avatar';
+import Logo from './Logo';
+import { UserContext } from './context/UserContext';
 
 const Chat = () => {
   const [ws, setWs] = useState(null);
   const [onlineUsers, setOnlineUsers] = useState({});
   const [selectedUserId, setSelectedUserId] = useState(null);
+
+  const { id } = useContext(UserContext);
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:5000/');
@@ -26,21 +30,30 @@ const Chat = () => {
     if ('online' in messageData) showOnlineUsers(messageData);
   };
 
+  // show all online users except logged in user
+  const onlineUsersExcludingLoggedInUser = { ...onlineUsers };
+  delete onlineUsersExcludingLoggedInUser[id];
+
   return (
     <div className="flex h-screen">
       <div className="bg-white w-1/3">
-        <div className="text-blue-600 font-bold flex gap-2 mb-4 p-4">
-          <HiOutlineChat className="w-6 h-6" />
-          <h3>Chatty</h3>
-        </div>
-        {Object.keys(onlineUsers).map((userId) => (
+        <Logo />
+        {Object.keys(onlineUsersExcludingLoggedInUser).map((userId) => (
           <div
+            key={userId}
             onClick={() => setSelectedUserId(userId)}
-            className={`border-b border-gray-100 py-2 px-4 flex items-center
-            gap-2 cursor-pointer ${selectedUserId === userId && 'bg-blue-50 transition-all shadow-sm'}`}
+            className={`border-b border-gray-100 flex items-center
+            gap-2 cursor-pointer ${
+              selectedUserId === userId && 'bg-blue-50 transition-all shadow-sm'
+            }`}
           >
-            <Avatar userId={userId} username={onlineUsers[userId]} />
-            <p className="text-gray-800 font-medium">{onlineUsers[userId]}</p>
+            {selectedUserId === userId && (
+              <div className="w-1 h-12 bg-blue-500 rounded-r-md" />
+            )}
+            <div className="flex gap-2 items-center py-2 px-4">
+              <Avatar userId={userId} username={onlineUsers[userId]} />
+              <p className="text-gray-800 font-medium">{onlineUsers[userId]}</p>
+            </div>
           </div>
         ))}
       </div>
