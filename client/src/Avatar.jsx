ef20b69f7c6556d68c570135a99ed9a11b8719ec
codepanther <pezechigozirim@gmail.com>
2023-09-27 10:54:09 +0100

    Update online and offline indicator

--- client/src/Avatar.jsx
+++ client/src/Avatar.jsx
@@ -20,12 +20,10 @@ const Avatar = ({ userId, username, online = false }) => {
       flex justify-center items-center font-bold relative`}
     >
       <p className="text-lg opacity-70">{username[0]}</p>
-      {online && (
-        <div
-          className="absolute w-3 h-3 bg-green-600
-        bottom-0 right-0 rounded-full border-white border"
-        />
-      )}
+      <div
+        className={`absolute w-3 h-3 ${online ? 'bg-green-600' : 'bg-gray-400'} 
+      bottom-0 right-0 rounded-full border-white border`}
+      />
     </div>
   );
 };
